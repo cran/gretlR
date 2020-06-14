03ed1befbea000d3afcc8b6d596bcef7bb612be1
Sagiru Mati <smati@smati.com.ng>
2020-06-14 15:10:02 +0000

    version 0.1.0

---
+++ inst/doc/gretlR.R
@@ -0,0 +1,25 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup1, include=FALSE----------------------------------------------------
+library(gretlR)
+
+## ----installation,eval=FALSE--------------------------------------------------
+#  install.packages("gretlR")
+#  
+#            OR
+#  
+#  devtools::install_github('sagirumati/gretlR')
+
+## ----scatter,eval=F,echo=T,fig.cap="Scatter graph produced by gretl chunk",out.width='\\textwidth'----
+#  library(knitr)
+#  knitr::include_graphics("scatter.png")
+#  
+
+## ----line,eval=F,eval=F,echo=T,out.width='\\textwidth',fig.cap="Line graph produced by gretl chunk"----
+#  knitr::include_graphics("line.png")
+#  
+
